d8919c1ca7466c8f74dd23133f789e85160aabfb
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-09-20 14:48:21 +0700

    Update load_modules.sh

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -2,13 +2,15 @@
 
 load_panel()
 {
-      	insmod /lib/modules/adaptive-ts.ko
-      	insmod /lib/modules/omnivision_td4160.ko
-        insmod /lib/modules/tran_perf_util.ko
+    insmod /lib/modules/adaptive-ts.ko
+    insmod /lib/modules/mtk-kpd.ko
+    insmod /lib/modules/pinctrl-mt6768.ko
+    insmod /lib/modules/i2c-mt65xx.ko
+    insmod /lib/modules/omnivision_td4160.ko
+    insmod /lib/modules/tran_perf_util.ko
 }
 
-
 load_panel
-wait 1
+sleep 1
 setprop modules.loaded 1
 exit 0
